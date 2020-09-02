1ddc9dfa9c3bb8687ad3007ba8d139d93cf3b1ac
Nick Horton <nhorton04@gmail.com>
2020-09-01 20:25:43 -0700

    added functionality to swap both ways

--- app.py
+++ app.py
@@ -40,37 +40,44 @@ def main():
 
     image1_arr = np.array(image1)
     image2_arr = np.array(image2)
+
     # Select src face
     src_points, src_shape, src_face = select_face(image1_arr)
+    src_points1, src_shape1, src_face1 = select_face(image2_arr)
     # Select dst face
     dst_points, dst_shape, dst_face = select_face(image2_arr)
+    dst_points1, dst_shape1, dst_face1 = select_face(image1_arr)
 
     if src_points is None or dst_points is None:
-        print('Detect 0 Face !!!')
+        print('No Face Detected')
         exit(-1)
 
     output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, image2_arr)
 
-    if st.button('Swap Faces'):
+    output2 = face_swap(src_face1, dst_face1, src_points1, dst_points1, dst_shape1, image1_arr)
+
+    if st.button('Swap Faces ⬇️'):
         st.image(output)
 
+    if st.button('Swap Faces ⬆️'):
+        st.image(output2)
 
 
-@st.cache(allow_output_mutation=True, hash_funcs=TL_GAN_HASH_FUNCS)
-def load_model():
-    """
-    Create the tensorflow session.
-    """
-    # Open a new TensorFlow session.
-    config = tf.ConfigProto(allow_soft_placement=True)
-    session = tf.Session(config=config)
-
-    # Must have a default TensorFlow session established in order to initialize the GAN.
-    with session.as_default():
-        # Read in either the GPU or the CPU version of the GAN
-        with open(MODEL_FILE_GPU if USE_GPU else MODEL_FILE_CPU, 'rb') as f:
-            G = pickle.load(f)
-    return session, G
+# @st.cache(allow_output_mutation=True, hash_funcs=TL_GAN_HASH_FUNCS)
+# def load_model():
+#     """
+#     Create the tensorflow session.
+#     """
+#     # Open a new TensorFlow session.
+#     config = tf.ConfigProto(allow_soft_placement=True)
+#     session = tf.Session(config=config)
+#
+#     # Must have a default TensorFlow session established in order to initialize the GAN.
+#     with session.as_default():
+#         # Read in either the GPU or the CPU version of the GAN
+#         with open(MODEL_FILE_GPU if USE_GPU else MODEL_FILE_CPU, 'rb') as f:
+#             G = pickle.load(f)
+#     return session, G
 
 if __name__ == "__main__":
     main()
