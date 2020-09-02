8cb3224452e581fc50a98f9ed2f562b41f068cb5
Nick Horton <nhorton04@gmail.com>
2020-09-01 20:59:59 -0700

    added try except block for before pics are uploaded

--- app.py
+++ app.py
@@ -23,45 +23,47 @@ TL_GAN_HASH_FUNCS = {
 }
 
 def main():
+    try:
+        #Upload images
+        uploaded_file = st.file_uploader("Choose a picture", type=['jpg', 'png'])
+        if uploaded_file is not None:
+            st.image(uploaded_file, width=200)
+        second_uploaded_file = st.file_uploader("Choose another picture", type=['jpg', 'png'])
+        if second_uploaded_file is not None:
+            st.image(second_uploaded_file, width=200)
 
-    #Upload images
-    uploaded_file = st.file_uploader("Choose a picture", type=['jpg', 'png'])
-    if uploaded_file is not None:
-        st.image(uploaded_file, width=200)
-    second_uploaded_file = st.file_uploader("Choose another picture", type=['jpg', 'png'])
-    if second_uploaded_file is not None:
-        st.image(second_uploaded_file, width=200)
+        # Read images to opencv
+        # src_img = cv2.imread(uploaded_file)
+        # dst_img = cv2.imread(second_uploaded_file)
+        image1 = Image.open(uploaded_file)
+        image2 = Image.open(second_uploaded_file)
 
-    # Read images to opencv
-    # src_img = cv2.imread(uploaded_file)
-    # dst_img = cv2.imread(second_uploaded_file)
-    image1 = Image.open(uploaded_file)
-    image2 = Image.open(second_uploaded_file)
+        image1_arr = np.array(image1)
+        image2_arr = np.array(image2)
 
-    image1_arr = np.array(image1)
-    image2_arr = np.array(image2)
+        # Select src face
+        src_points, src_shape, src_face = select_face(image1_arr)
+        src_points1, src_shape1, src_face1 = select_face(image2_arr)
+        # Select dst face
+        dst_points, dst_shape, dst_face = select_face(image2_arr)
+        dst_points1, dst_shape1, dst_face1 = select_face(image1_arr)
 
-    # Select src face
-    src_points, src_shape, src_face = select_face(image1_arr)
-    src_points1, src_shape1, src_face1 = select_face(image2_arr)
-    # Select dst face
-    dst_points, dst_shape, dst_face = select_face(image2_arr)
-    dst_points1, dst_shape1, dst_face1 = select_face(image1_arr)
+        if src_points is None or dst_points is None:
+            print('No Face Detected')
+            exit(-1)
+        output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, image2_arr)
 
-    if src_points is None or dst_points is None:
-        print('No Face Detected')
-        exit(-1)
+        output2 = face_swap(src_face1, dst_face1, src_points1, dst_points1, dst_shape1, image1_arr)
 
-    output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, image2_arr)
 
-    output2 = face_swap(src_face1, dst_face1, src_points1, dst_points1, dst_shape1, image1_arr)
+    except:
+        st.title('No Images Uploaded Yet')
 
     if st.button('Swap Faces ⬇️'):
         st.image(output)
 
     if st.button('Swap Faces ⬆️'):
         st.image(output2)
-
 
 # @st.cache(allow_output_mutation=True, hash_funcs=TL_GAN_HASH_FUNCS)
 # def load_model():
