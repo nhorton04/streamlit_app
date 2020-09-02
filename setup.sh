a19cc22bd889f25df53ce0344938d650bdf05630
Nick Horton <nhorton04@gmail.com>
2020-09-01 21:30:44 -0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"nhorton04@gmail.com\"\n\
+echo "[general]
+email = \"nhorton04@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
